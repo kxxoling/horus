3fba63784b83c24a88a4d26606f22865122c806e
Kane Blueriver <kxxoling@gmail.com>
2015-04-21 10:42:20 +0800

    Set debug mode to True in development

--- run.py
+++ run.py
@@ -4,9 +4,8 @@ from horus.apps import create_app
 
 config_file = os.path.join(os.path.dirname(os.path.realpath(__file__))
                            , 'config.py')
-print config_file
 app = create_app(config_file)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
