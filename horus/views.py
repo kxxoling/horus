a07038347e23255dbab443f12905791d2be85d9a
Kane Blueriver <kxxoling@gmail.com>
2015-04-21 15:15:00 +0800

    Add a simple resume edit page

--- horus/views.py
+++ horus/views.py
@@ -8,3 +8,16 @@ frontend_views = Blueprint('frontend', __name__, url_prefix='/')
 @frontend_views.route('/')
 def index():
     return render_template('index.jade', page_title='Welcome to Horus')
+
+
+@frontend_views.route('resume/<int:resume_id>')
+def resume(resume_id=None):
+    if resume_id:
+        print resume_id
+    return render_template('resume.jade', content="""# Resume
+
+sample resume
+
+## Content
+
+This is content!""")
