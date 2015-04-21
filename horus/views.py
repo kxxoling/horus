db168d8a6c8dd5ccded59f728bff310099f15478
Kane Blueriver <kxxoling@gmail.com>
2015-04-21 18:04:17 +0800

    Simple design for ResumeList

--- horus/views.py
+++ horus/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import render_template
+from .models import Resume
 
 
 frontend_views = Blueprint('frontend', __name__, url_prefix='/')
@@ -10,14 +11,14 @@ def index():
     return render_template('index.jade', page_title='Welcome to Horus')
 
 
-@frontend_views.route('resume/<int:resume_id>')
-def resume(resume_id=None):
-    if resume_id:
-        print resume_id
-    return render_template('resume.jade', content="""# Resume
+@frontend_views.route('resume/')
+def list_resumes():
+    resume_list = Resume.query.all()
+    return render_template('resume_list.jade', resume_list=resume_list)
 
-sample resume
 
-## Content
+@frontend_views.route('resume/<int:resume_id>/')
+def show_resume(resume_id=None):
+    resume = Resume.query.get_or_404(resume_id)
 
-This is content!""")
+    return render_template('resume.jade', resume=resume)
